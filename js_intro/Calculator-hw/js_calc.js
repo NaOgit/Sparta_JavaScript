ecab65110e95c89b8c8631a82ae6718957f2f73b
NaO <noukongka@spartaglobal.com>
2018-08-07 10:58:32 +0100

    Files created

--- js_intro/Calculator-hw/js_calc.js
+++ js_intro/Calculator-hw/js_calc.js
@@ -1,22 +1,13 @@
+function myFunction() {
 //Declaring variables
-  function myFunction() {
   var x,y,z,oper;
-  x=prompt("Type a number");
-  oper=prompt("Type an operator");
-  y=prompt("Type a number");
+  x=prompt("Enter a number");
+  oper=prompt("Enter an operator");
+  y=prompt("Enter a number");
 
 //Conditions
   if(oper=="+"){
     z=Number(x)+Number(y);
-  }
-  else if(oper=!null){
-    z="Error"
-  }
-  else if(x=!null){
-    z="Error"
-  }
-  else if(y=!null){
-    z="Error"
   }
   else if(oper=="/"){
     z=Number(x)/Number(y);
@@ -26,6 +17,15 @@
   }
   else if(oper=="-"){
     z=Number(x)-Number(y);
+  }
+  else if(oper=!null){
+    z="Error"
+  }
+  else if(x=!null){
+    z="Error"
+  }
+  else if(y=!null){
+    z="Error"
   }
 //Popup return value
   alert(z);

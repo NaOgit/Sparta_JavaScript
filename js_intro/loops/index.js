a4f1ae9951c631c12e01810f79297b7afc909889
NaO <noukongka@spartaglobal.com>
2018-08-08 12:12:53 +0100

    loops

---
+++ js_intro/loops/index.js
@@ -0,0 +1,47 @@
+//loops stuff
+
+// for loops
+
+// for (var i =0; i < 10; i++) {
+//   // console.log("This is a loop" + i);
+//   var myArray = ["Natt", "is ", "amazing"]
+// }
+//
+// for(var i=0; i < myArray.length; i++){
+//   console.log(myArray[i]);
+// }
+
+// i = 0 => starting position/point
+//i >/< => number of Loops
+//i++ => incrementation
+
+//while Loops
+
+// var i=0;
+// while (i < 5) {
+//   console.log("This loop has run " + i + " times");
+//   i++;
+// }
+
+//do while
+// var number;
+//
+// do {
+//   number = Math.random();
+//   console.log(number);
+// } while (number < 0.5)
+
+//for in Loops
+
+// var myObject = new Object();
+// //key and value
+// myObject.a = "1";
+// myObject.b = "2";
+// myObject.c = "3";
+//
+// for (var key in myObject) {
+//   console.log(key);
+//   console.log(myObject[key]);
+// }
+
+//Object has key and value
